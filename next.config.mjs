b7120567fa4cfed9e931d8aefdb4372a0788e002
Ismael Boukhars <ismael.boukhars@gmail.com>
2024-05-23 12:38:17 +0200

    redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/:path*',
+        destination: 'https://www.akuocoop.com/:path*',
+        basePath: false,
+
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
